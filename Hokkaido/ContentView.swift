fada6b0cd14f601a8699b580779583b488d384e1
jamescarter2001 <jamesmcarter7@gmail.com>
2022-02-20 16:49:05 +0000

    Initial Commit

---
+++ Hokkaido/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hokkaido
+//
+//  Created by James on 20/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hokkaido/HokkaidoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HokkaidoApp.swift
+//  Hokkaido
+//
+//  Created by James on 20/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct HokkaidoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
